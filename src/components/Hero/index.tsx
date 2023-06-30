43e5aa6fbe2911445db065db4b50593962b072cb
Walber Vaz da Silva <wvs.walber@gmail.com>
2023-06-30 16:20:39 -0300

    feat: finish section hero display desktop

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,4 +1,5 @@
 import hero from '../../assets/hero.png';
+import Container from '../Container';
 import Menu from '../Menu';
 
 export default function Hero(): JSX.Element {
@@ -12,6 +13,20 @@ export default function Hero(): JSX.Element {
       }}
     >
       <Menu />
+      <Container>
+        <div className='hero__content'>
+          <h1 className='hero__title'>
+            <span className='hero__title-desc'>Pronto para treinar</span>
+            <span className='hero__subtitle-desc'>seu corpo e mente?</span>
+          </h1>
+          <p className='hero__description'>
+            O treinamento na academia é uma abordagem estruturada e disciplinada
+            do exercício físico que se concentra na força, resistência e
+            melhoria geral do condicionamento físico.
+          </p>
+          <button className='hero__button'>Matricule-se agora</button>
+        </div>
+      </Container>
     </section>
   );
 }
